b5152e4128e04a658e3a96e93c87a2a2aac6280f
WINSON ARAVINTH RAJ C <winsonaravinthraj@gmail.com>
2025-07-25 23:57:23 +0530

    ahhhhhhhhh

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://curd-operation-gocn.onrender.com/api/auth',
+  baseURL: 'https://curd-operation-gocn.onrender.com',
   withCredentials: true,
 });
 
