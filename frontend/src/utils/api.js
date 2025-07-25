dc1e4e1821cd201bf484f71236b5f898fa5ca66e
WINSON ARAVINTH RAJ C <winsonaravinthraj@gmail.com>
2025-07-25 23:54:06 +0530

    env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'process.env.REACT_APP_API_BASE',
+  baseURL: 'https://curd-operation-gocn.onrender.com/api/auth',
   withCredentials: true,
 });
 
